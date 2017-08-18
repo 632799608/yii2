6b5d88bf13f650b72c1857bbc420cd52812ee7f4
632799608 <632799608@qq.com>
2017-08-18 17:07:56 +0800

    first commit

---
+++ admin/views/test/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = 666;
+
+?>
+<?= Html::tag('p', Html::encode('周志红'), ['class' => 'username']) ?>
---
+++ common/models/Users.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $status
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['status'], 'integer'],
+            [['name', 'created_at', 'updated_at'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+}
